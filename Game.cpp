b065e474504c6b9e89a464e79854e6c84d4bedd7
Rutger Hertoghe <rutgerh@hotmail.be>
2021-12-03 14:35:55 +0100

    Deleted printed text from start

--- Game.cpp
+++ Game.cpp
@@ -19,10 +19,7 @@ void Start()
 	InitPlayer(g_Player, g_CellArr);
 	InitInteractables();
 	InitEnemies(g_EnemyArr, g_EnemyArrSize, g_CellArr, g_GridSize);
-	std::cout << "hello Tanguy" << std::endl;
-	std::cout << "a change that I add at 10:05AM, 3rd Dec" << std::endl;
-	std::cout << "a change that I add at 10:12AM, 3rd Dec" << std::endl;
-	std::cout << "a change that I add at 10:14AM, 3rd Dec" << std::endl;
+
 }
 
 void Draw()
